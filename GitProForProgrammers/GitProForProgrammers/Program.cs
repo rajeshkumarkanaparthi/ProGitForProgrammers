bd3770cf5c6e6f5be23c6e38a249ceaf190a47d7
Rajesh Kumar Kanaparthi <rajeshkumar.kanaparthi@outlook.com>
2023-03-03 15:57:48 -0600

    First commit - from command line

---
+++ GitProForProgrammers/GitProForProgrammers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitProForProgrammers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
